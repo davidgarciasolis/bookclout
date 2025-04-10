9f4778f433838ae82b8cdc8edeb12d7e55760756
noname <davidgarciasolis@gmail.com>
2025-04-10 03:53:41 +0200

    Reorganizar el código en inicio.php: mejorar la legibilidad y agregar comentarios para las consultas de reservas y préstamos

--- manager/inicio.php
+++ manager/inicio.php
@@ -1,5 +1,9 @@
-<!-- Comprueba que la sesión está activa -->
-<?php include '../autenticacion/check_sesion.php'; ?>
+<?php 
+// Comprueba que la sesión está activa
+include '../autenticacion/check_sesion.php'; 
+require '../autenticacion/conexion.php'; 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,12 +13,40 @@
     <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
-    <!-- header -->
-    <?php include 'includes/header.php';?>
+    <!-- Header -->
+    <?php include 'includes/header.php'; ?>
+
     <main>
-        <h1>Bienvenido <?php echo $_SESSION["usuario"]; ?></h1>
+        <h1>Resumen</h1>
+        <?php
+
+        // Consulta para obtener reservas activas
+        $emailUsuario = $_SESSION['email'];
+        $sqlReservas = "SELECT COUNT(*) AS total_reservas 
+                        FROM reservas 
+                        WHERE estado = 'activa'";
+        $resultReservas = $conn->query($sqlReservas);
+        $reservasActivas = ($resultReservas->num_rows > 0) ? $resultReservas->fetch_assoc()["total_reservas"] : 0;
+
+        // Consulta para obtener préstamos no devueltos
+        $sqlPrestamos = "SELECT COUNT(*) AS total_prestamos 
+                         FROM prestamos 
+                         WHERE fecha_devolucion IS NULL";
+        $resultPrestamos = $conn->query($sqlPrestamos);
+        $prestamosNoDevueltos = ($resultPrestamos->num_rows > 0) ? $resultPrestamos->fetch_assoc()["total_prestamos"] : 0;
+
+        // Mostrar resumen
+        echo"<p><strong>Reservas Activas:</strong> $reservasActivas</p>
+            <p><strong>Préstamos No Devueltos:</strong> $prestamosNoDevueltos</p>";
+        ?>
     </main>
-    <!-- footer -->
-    <?php include 'includes/footer.php';?>
+
+    <!-- Footer -->
+    <?php include 'includes/footer.php'; ?>
+
+    <?php
+    // Cierra la conexión a la base de datos
+    $conn->close();
+    ?>
 </body>
 </html>
