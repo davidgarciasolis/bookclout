7ef5624b955d0ee47f0859d784a410df2b4b128d
noname <davidgarciasolis@gmail.com>
2025-04-23 13:32:23 +0200

    Agregar verificación de permisos de administrador en la sesión de usuario

--- autenticacion/check_sesion.php
+++ autenticacion/check_sesion.php
@@ -4,4 +4,26 @@ if (!isset($_SESSION["email"])) {
     header("Location: login.php");
     exit();
 }
+
+include 'conexion.php';
+
+if (isset($_SESSION['email'])) {
+    $email = $_SESSION['email'];
+    $query = "SELECT admin FROM usuarios WHERE email = '$email'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if ($row['admin'] != 1) {
+            echo "<p>No tienes permiso para acceder a esta página.</p>";
+            echo "<form action='../autenticacion/logout_user.php' method='POST'>";
+            echo "<button type='submit'>Volver</button>";
+            echo "</form>";
+            exit();
+        }
+    } else {
+        echo "<p>Error al verificar los permisos del usuario.</p>";
+        exit();
+    }
+}
 ?>
