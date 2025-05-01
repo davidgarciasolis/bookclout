69b9041f94503c3b3b346c07e49dfd225cf6c873
noname <davidgarciasolis@gmail.com>
2025-05-01 10:46:10 +0200

    Agregar estilos y estructura al encabezado: implementar contenedor y navegación responsiva

--- includes/header.php
+++ includes/header.php
@@ -1,22 +1,24 @@
 <?php
 session_start();
 ?>
-<header>
-    <div>
+<header class="main-header">
+    <div class="header-container">
         <!-- Logo -->
         <link rel="icon" type="image/png" href="img/logo.png">
-        <h1><a href="index.php">Bookclout</a></h1>
+        <h1 class="logo"><a href="index.php">Bookclout</a></h1>
 
         <!-- Opciones -->
-        <ul>
-            <li><a href="index.php">Inicio</a></li>
-            <li><a href="catalogo.php">Catalogo</a></li>
-            <li><a href="reservas_prestamos.php">Reservas y prestamos</a></li>
-            <?php if (isset($_SESSION['email'])): ?>
-                <li><a href="autenticacion/logout_user.php">Cerrar sesión</a></li>
-            <?php else: ?>
-                <li><a href="login.php">Login</a></li>
-            <?php endif; ?>
-        </ul>
+        <nav class="main-nav">
+            <ul class="nav-list">
+                <li class="nav-item"><a href="index.php">Inicio</a></li>
+                <li class="nav-item"><a href="catalogo.php">Catalogo</a></li>
+                <li class="nav-item"><a href="reservas_prestamos.php">Reservas y prestamos</a></li>
+                <?php if (isset($_SESSION['email'])): ?>
+                    <li class="nav-item"><a href="autenticacion/logout_user.php">Cerrar sesión</a></li>
+                <?php else: ?>
+                    <li class="nav-item"><a href="login.php">Login</a></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
     </div>
 </header>
