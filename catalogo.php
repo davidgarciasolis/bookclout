6220203de5f6fe395947f1d759e3b84ab15983ff
noname <davidgarciasolis@gmail.com>
2025-05-03 08:20:44 +0200

    Reorganizar la estructura del documento HTML y asegurar la inclusión del encabezado y pie de página

--- catalogo.php
+++ catalogo.php
@@ -1,12 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Catalogo</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    
+
+
 <?php
-include 'includes/header.php';
 include 'autenticacion/conexion.php';
 ?>
 
-<link rel="stylesheet" href="css/styles.css">
+
 
 
 <?php
+include 'includes/header.php';
 // Pagination logic
 $limit = 10;
 $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
@@ -111,3 +123,5 @@ $total_pages = ceil($total_books / $limit);
 </script>
 
 <?php include 'includes/footer.php'; ?>
+</body>
+</html>
