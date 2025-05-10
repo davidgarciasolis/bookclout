1f9d3bb829b6029a5ae103175f3ac71aa4c8c249
noname <davidgarciasolis@gmail.com>
2025-05-10 08:43:04 +0200

    se envia un correo cuando se crea un usuario

--- manager/php/procesar_alta_usuario.php
+++ manager/php/procesar_alta_usuario.php
@@ -1,6 +1,9 @@
 <?php
 require '../../autenticacion/check_sesion.php';
 require '../../autenticacion/conexion.php';
+require '../../vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 try {
     // Obtener datos del formulario
@@ -21,6 +24,32 @@ try {
         if ($stmt->execute()) {
             $_SESSION['mensaje'] = "Usuario creado exitosamente.";
             $_SESSION['mensaje_tipo'] = "exito";
+
+            // Enviar correo al usuario
+            $mail = new PHPMailer(true);
+            try {
+                // Configuración del servidor SMTP
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com'; // Cambiar por el host SMTP
+                $mail->SMTPAuth = true;
+                $mail->Username = 'bookcloud.no.reply@gmail.com'; // Cambiar por tu correo
+                $mail->Password = 'hzir heql ozut zpht'; // Cambiar por tu contraseña
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port = 587;
+                $mail->SMTPDebug = 2; // Habilitar el modo de depuración para obtener más detalles
+                $mail->Debugoutput = 'error_log'; // Registrar la salida en el log de errores
+
+                // Configuración del correo
+                $mail->setFrom('bookcloud.no.reply@gmail.com', 'Bookcloud'); // Cambiar por tu correo y nombre
+                $mail->addAddress($email, $nombre);
+                $mail->isHTML(true);
+                $mail->Subject = 'Bienvenido a nuestra plataforma';
+                $mail->Body = '<h1>Hola ' . $nombre . '!</h1><p>Tu cuenta ha sido creada exitosamente.</p>';
+
+                $mail->send();
+            } catch (Exception $e) {
+                error_log("Error al enviar el correo: " . $mail->ErrorInfo);
+            }
         } else {
             $_SESSION['mensaje'] = "El usuario no se ha creado. Por favor, intenta nuevamente.";
             $_SESSION['mensaje_tipo'] = "error";
