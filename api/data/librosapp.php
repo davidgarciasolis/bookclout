8499d66c4f13d7c2ee8ce0659220c94546cefbf5
TyroneDAM <149711621+TyroneDAM@users.noreply.github.com>
2025-05-13 21:17:10 +0200

    libros app
    Para que los libros se puedan ver en la app ya que no es compatible

---
+++ api/data/librosapp.php
@@ -0,0 +1,29 @@
+<?php
+require_once "../../autenticacion/conexion.php";
+require_once "../auth/validate.php"; //Para el toekn
+
+header("Content-Type: application/json");
+
+$sql = "SELECT isbn, titulo, autor, editorial, fecha_publicacion, portada, descripcion, unidades, genero FROM libros";
+$result = $conn->query($sql);
+
+$libros = [];
+
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $libros[] = [
+            "isbn" => $row["isbn"],
+            "titulo" => $row["titulo"],
+            "autor" => $row["autor"],
+            "editorial" => $row["editorial"],
+            "fecha_publicacion" => $row["fecha_publicacion"],
+            "portada" => $row["portada"], 
+            "descripcion" => $row["descripcion"],
+            "unidades" => $row["unidades"],
+            "genero" => $row["genero"]
+        ];
+    }
+}
+
+echo json_encode($libros, JSON_UNESCAPED_UNICODE);
+$conn->close();
