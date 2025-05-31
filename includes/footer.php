f9c2d71b57a42c2e9f506b54bb2c43421920888d
TyroneDAM <149711621+TyroneDAM@users.noreply.github.com>
2025-05-31 17:24:50 +0200

    cambio el footer a algo más bonito

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,21 @@
-<footer>
-    <h1><a href="index.php">Bookcloud</a></h1>
-</footer>
+footer {
+    background-color: #014421; /* Verde bosque */
+    color: white;
+    text-align: center;
+    padding: 20px;
+}
+
+footer h1 {
+    margin: 0;
+    font-size: 1.2rem;
+}
+
+footer a {
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+footer a:hover {
+    text-decoration: underline;
+}
